7df78b064f49f4de6f6623813e7486c652877077
shiva moeini <shivamoeini2000@gmail.cpm>
2022-09-15 12:34:17 +0430

    card.fetch

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-const searchBtn=document.getElementById("search-btn");
-const mealList=document.getElementById("meal");
-const mealDetailsContent=document.querySelector('.meal-details-content');
-const recipeCloseBtn=document.getElementById("recipe-close-btn");
+const searchBtn = document.getElementById("search-btn");
+const mealList = document.getElementById("meal");
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById("recipe-close-btn");
 
 
 // event listeners
 
-searchBtn.addEventListener('click',getMealList);
+searchBtn.addEventListener('click', getMealList);
 
 // get meal list that matches with the intgredients
 function getMealList() {
-    let searchInputTex=document.getElementById('search-input').value.trim();
+    let searchInputTex = document.getElementById('search-input').value.trim();
     // console.log(searchInputTex.length);
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTex}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data);
+            let html = "";
+            if (data.meals) {
+                data.meals.forEach(meal => {
+                   html += `
+                <div class="meal-item" data-id="${meal.idMeal}">
+                  <div class="meal-img">
+                  <img src="${meal.strMealThumb}" alt="food">
+                  </div>
+                   <div class="meal-name">
+                    <h3>${meal.strMeal}</h3>
+                    <a href="#" class="recipe-btn">Get Recipe</a>
+                    </div>
+            </div>
+                
+                `
+                });
+            }
+            mealList.innerHTML=html;
+        });
 }
